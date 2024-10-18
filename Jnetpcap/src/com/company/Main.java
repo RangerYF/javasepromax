0a859288794b8cf920bef3a4983f2a380a588bd8
Wuyifei <2107376513@qq.com>
2024-10-18 09:55:25 +0800

    first change!

--- Jnetpcap/src/com/company/Main.java
+++ Jnetpcap/src/com/company/Main.java
@@ -2,21 +2,15 @@ package com.company;
 
 import com.company.View.AppFrame;
 
-/**
- * @ClassName Main  //类名称
- * @Description: 类描述 //程序入口
- * @Author: 程哥哥    //作者
- * @CreateDate: 2022/3/25 23:12	//创建时间
- * @UpdateUser: 更新人 程哥哥
- * @UpdateDate: 2022/3/29 20:29	//更新时间
- * @UpdateRemark: 更新的信息
- * @Version: 1.3    //版本号
- */
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         new AppFrame().dataInjection();
+
+
+
     }
 }
 
